d2083238cef95658fcd09989f612a65c2a68bd28
janaali17 <ja2200300@tkh.edu.eg>
2024-12-01 21:35:08 +0200

    ..

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const express =require('express'); //assigning require express to express
-const app= express()
-const port= 7777 //assigning the port num to 7777
+const express = require('express'); //assigning require express to express
+const app = express()
+const port = 7777 //assigning the port num to 7777
 app.use(express.json) //change to readable form
-const sqlite3= require('sqlite3') //imported sqlite(database) 
-const db = new sqlite3.Database('inputs.db')  
+const sqlite3 = require('sqlite3') //imported sqlite(database) 
+const data = new sqlite3.Database('inputs.data')
+
+
+app.listen(port,()=>{
+    console.log(`Server is running at port: ${port}`)
+})
+
